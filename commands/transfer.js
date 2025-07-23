d403a2ec9b1f5c72f20059caa80b003f9c02126b
didil <faridfadilah70@gmail.com>
2025-07-23 11:42:28 +0700

    Update transfer. fixing bug at 33

--- commands/transfer.js
+++ commands/transfer.js
@@ -5,34 +5,42 @@ const bcrypt = require('bcrypt');
 const session = require('../session');
 
 async function transfer(name) {
-    let nama = readline.question('Masukkan nama akun tujuan: ');
-    let amount = parseInt(readline.question('Masukkan jumlah uang yang akan di transfer: '));
-    const [tujuan] = await db.execute(
+    let nama = readline.question('Masukkan nama akun penerima: ');
+    let amount = parseFloat(readline.question('Masukkan jumlah uang yang akan di transfer: '));
+    let [penerima] = await db.execute(
         'SELECT * FROM accounts WHERE name = ?', [nama]
     );
-    if (!tujuan[0] || tujuan[0].length === 0) {
+    let id_penerima = penerima[0].id; //ID penerimaNYA
+    if (!penerima[0] || penerima[0].length === 0) {
         console.log('Gak ada akunnya');
         exit;
     }
-    let [result] = await db.execute(
+    console.log(penerima)
+    let [pengirim] = await db.execute(
         'SELECT * FROM accounts WHERE name=?', [name]
     );
-    if (amount > result[0].balance) {
+    let id_pengirim = pengirim[0].id; //ID AKUN
+    if (amount > pengirim[0].balance) {
         console.log("Saldo tidak mencukupi");
         exit;
     }else{
-        let sisauang = result[0].balance - amount;
-        [result] = await db.execute( //1 function
+        let sisauang = pengirim[0].balance - amount;
+        [pengirim] = await db.execute( //1 function
             'UPDATE accounts SET balance = ? WHERE name = ?', [sisauang, name]
         );
-        let uangtujuan = tujuan[0].balance + amount;
-        let [result2] = await db.execute(
-            'UPDATE accounts SET balance = ? WHERE name = ?', [uangtujuan, nama]
+        console.log('uang pengirim saat ini: ', sisauang)
+        let uangpenerima = parseInt(penerima[0].balance) + amount;
+        [penerima] = await db.execute(
+            'UPDATE accounts SET balance = ? WHERE name = ?', [uangpenerima, nama]
         )
-        const [result_trans] = await db.execute( //2 function
-            'INSERT INTO transactions (account_id, type, amount, target_id) VALUES (?, ?, ?, ?)', [result[0].id, 'transfer', amount, tujuan[0].id]
+        console.log('uang penerima saat ini: ', uangpenerima)
+        const [result_trans_pengirim] = await db.execute( //2 function
+            'INSERT INTO transactions (account_id, type, amount, target_id) VALUES (?, ?, ?, ?)', [id_pengirim, 'transfer_out', amount, id_penerima]
         );
-        if (!result && !result2 && !result_trans) {
+        const [result_trans_penerima] = await db.execute( //2 function
+            'INSERT INTO transactions (account_id, type, amount, target_id) VALUES (?, ?, ?, ?)', [id_penerima, 'transfer_in', amount, id_pengirim]
+        );
+        if (!pengirim && !penerima && !result_trans_pengirim && !result_trans_penerima) {
             console.log('Gagal melakukan transfer');
         } else {
             console.log('Berhasil melakukan transfer');
